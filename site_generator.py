8a2bd0d5c82f5e026eddc6ec259ca5c49205738d
TvoozMagnificent <luchang1106@icloud.com>
2023-02-22 20:29:37 +0800

    Created Bunch of Stuff
    Signed-off-by: TvoozMagnificent <luchang1106@icloud.com>

--- site_generator.py
+++ site_generator.py
@@ -1,23 +1,28 @@
 
 from glob import glob
 from re import sub
+from os import mkdir
 
 projects = ["test"]
 
 for project in projects:
-    site_url = f"TvoozMagnificent.github.io/{projects}/"
+    site_url = f"TvoozMagnificent.github.io/{project}/"
     source_directory = \
-        f"/Users/luchang/Desktop/pythonprojects/TvoozMagnificent.github.io/source/{projects}/"
+        f"/Users/luchang/Desktop/pythonprojects/TvoozMagnificent.github.io/source/{project}/"
     output_directory = \
-        f"/Users/luchang/Desktop/pythonprojects/TvoozMagnificent.github.io/{projects}/"
+        f"/Users/luchang/Desktop/pythonprojects/TvoozMagnificent.github.io/{project}/"
 
     filenames = {
         file[:-4]: open(file).read().strip().split('\n', 1)[0]
         for file in glob(f'{source_directory}*.txt')
     }
 
+    try: mkdir(output_directory)
+    except: pass
+
     for file in filenames:
-        text = "# " + open(f'{file}.txt').read().strip()
+        content = open(f'{file}.txt').read().strip().split('\n', 1)
+        text = "# " + content[0] + '\n\n\n\n\n' + content[1]
         for to_replace in filenames:
             text = text.replace(f'[{to_replace.split("/")[-1]}]',
                 f'[{filenames[to_replace]}]({to_replace.split("/")[-1]}.html)')
