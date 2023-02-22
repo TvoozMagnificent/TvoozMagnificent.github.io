7385e34bff19efd613735ff7a8631f03857725aa
TvoozMagnificent <luchang1106@icloud.com>
2023-02-22 20:12:24 +0800

    Created Bunch of Stuff

--- site_generator.py
+++ site_generator.py
@@ -17,6 +17,5 @@ for file in filenames:
     text = open(f'{file}.txt').read().strip().split('\n', 1)[1]
     for to_replace in filenames:
         text = text.replace(f'[{to_replace.split("/")[-1]}]',
-            f'[{filenames[to_replace]}]'
             f'<a href="{to_replace.split("/")[-1]}.html">{filenames[to_replace]}</a>')
     open(f'{file.replace("/source", "")}.html', 'w').write(text)
